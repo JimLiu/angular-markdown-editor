fb6436daec356a819b4f623fa45b22e569aad572
Sam Milledge <sam@sammilledge.com>
2014-06-22 15:01:04 +1000

    Debounce ng-bind-markdown to prevent flickering

--- dist/angular-markdown-editor.js
+++ dist/angular-markdown-editor.js
@@ -552,6 +552,21 @@
 })();
 (function() {
   'use strict';
+
+  // Debounce function taken from Underscore.js 
+  // (https://github.com/jashkenas/underscore)
+  var debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        timeout = null;
+        func.apply(context, args);
+      }, wait);
+    };
+  };
+
   angular.module('ui.markdown')
 
   .directive('ngBindMarkdown', function() {
@@ -559,16 +574,15 @@
       restrict: 'A',
       link: function(scope, element, attrs) {
         element.addClass('ng-binding-markdown').data('$bindingMarkdown', attrs.ngBindMarkdown);
-        scope.$watch(attrs.ngBindMarkdown, function bindMarkdownWatchAction(value) {
+        scope.$watch(attrs.ngBindMarkdown, debounce(function bindMarkdownWatchAction(value) {
           var converter = new Showdown.converter();
           var markdown = value || '';
           var htmlText = converter.makeHtml(markdown);
           element.html(htmlText);
-        });
+        }, 50));
       }
     };
 
   });
-
 
 })();
